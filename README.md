667f76929e4cce2e345784d87746797fdd9f70af
AN DO <anmana000@gmail.com>
2023-06-17 22:46:48 +0700

    added logging and separated into functions

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Routing-of-telephone-calls
+
 alaTest / ValueChecker (ICSS) challenge 2023 for anmana000@gmail.com
 
-
 Some telephone operators have submitted their price lists including price per minute for different phone number prefixes. The price lists look like this:
+
 ```
 Operator A:
 1	 0.9
@@ -21,9 +22,10 @@ Operator B:
 467	 1.0
 48	 1.2
 ```
+
 And so on...
 
-The left column represents the telephone prefix (country + area code) and the right column represents the operator's price per minute for a number starting with that prefix. When several prefixes match the same number, the longest one should be used. If you, for example, dial +46-73-212345 you will have to pay \$1.1/min with Operator A and \$1.0/min with Operator B. 
+The left column represents the telephone prefix (country + area code) and the right column represents the operator's price per minute for a number starting with that prefix. When several prefixes match the same number, the longest one should be used. If you, for example, dial +46-73-212345 you will have to pay \$1.1/min with Operator A and \$1.0/min with Operator B.
 
 If a price list does not include a certain prefix you cannot use that operator to dial numbers starting with that prefix. For example it is not possible to dial +44 numbers with operator A but it is possible with Operator B.
 
@@ -35,11 +37,19 @@ Telephone numbers should be inputted in the same format as in price lists, for e
 
 # Instructions
 
-Use your favorite language to solve the exercise. 
-Put your focus on code design and readability. 
-Do not use a database or create a GUI. 
-Plus is given to efficient solutions. 
+Use your favorite language to solve the exercise.
+Put your focus on code design and readability.
+Do not use a database or create a GUI.
+Plus is given to efficient solutions.
 The code should have unit test(s).
 Deliver to us via online repository (give access to erik.ohlzon@alatest.com). If your repo username does not indicate your name somehow, please make a note of whom the solution belongs to.
 Please include (in the repo) a current CV listing any recent or relevant code examples.
 
+# Checklist
+
+- [x] Algorithm
+- [x] Logging
+- [ ] Testing
+- [ ] CI CD github
+- [ ] Functions documentation
+- [ ] Restrict python version
--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from __future__ import annotations
-import csv
-from os import listdir
-from os.path import isfile, join
+from typing_extensions import TypedDict
+import logging
 from pprint import pprint
+from os.path import isfile, join
+from os import listdir
+import csv
+
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+    "%(asctime)s -  %(name)s: %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+
 CSV_FOLDER = "./operators/"
 
 
@@ -21,64 +34,82 @@ class DigitNode:
         # return f"DigitNode({self.digit}) @ {hex(id(self))}"
         return f"DigitNode({self.digit})"
 
-    # def insert(self, )
+
+class OperatorPrice(TypedDict):
+    price: float
+    operator: str
 
 
-def query(first_digit, phone_number):
-    assert phone_number
-    node = first_digit[int(phone_number[0])]
-    for digit in phone_number[1:]:
-        digit = int(digit)
-        if digit in node.next_digit:
-            node = node.next_digit[digit]
-        else:
-            return node.cheapest_operator, node.cheapest_price, node.digit
+class CheapestOpFinder:
 
+    def __init__(self, operator_folder_path: str) -> None:
+        assert operator_folder_path
+        self.path: str = operator_folder_path
+        self.first_digit: dict[int, DigitNode] = dict()
+        self.minimum_price_dict: dict[str, OperatorPrice] = dict()
 
-if __name__ == "__main__":
-    minimum_price_dict = dict()
-    files = [join(CSV_FOLDER, f) for f in listdir(CSV_FOLDER)
-             if isfile(join(CSV_FOLDER, f))]
+        self._sort_prefix_price()
+        self._build_minimum_price_dict()
 
-    for i, file in enumerate(files):
-        with open(file, newline='') as csv_file:
-            reader = csv.reader(csv_file, delimiter=',')
-            for prefix, price in reader:
-                if prefix in minimum_price_dict:
-                    if price < minimum_price_dict[prefix]["price"]:
-                        minimum_price_dict[prefix]["price"] = price
-                        minimum_price_dict[prefix]["operator"] = file
-                else:
-                    minimum_price_dict[prefix] = {
-                        "price": price,
-                        "operator": file
-                    }
-    pprint(minimum_price_dict)
+    def _sort_prefix_price(self) -> None:
+        files = [join(self.path, f) for f in listdir(self.path)
+                 if isfile(join(self.path, f))]
 
-    first_digit = {}
-    for i in range(10):
-        first_digit[i] = DigitNode(i)
+        for file in files:
+            with open(file, newline='') as csv_file:
+                reader = csv.reader(csv_file, delimiter=',')
+                for prefix, price in reader:
+                    if prefix in self.minimum_price_dict:
+                        if price < self.minimum_price_dict[prefix]["price"]:
+                            self.minimum_price_dict[prefix]["price"] = price
+                            self.minimum_price_dict[prefix]["operator"] = file
+                    else:
+                        self.minimum_price_dict[prefix]: OperatorPrice = {
+                            "price": price,
+                            "operator": file
+                        }
+        pprint(self.minimum_price_dict)
 
-    for prefix in minimum_price_dict.keys():
-        price = minimum_price_dict[prefix]["price"]
-        operator = minimum_price_dict[prefix]["operator"]
+    def _build_minimum_price_dict(self) -> None:
+        for i in range(10):
+            self.first_digit[i] = DigitNode(i)
 
-        assert prefix and price and operator
-        current_node = first_digit[int(prefix[0])]
+        for prefix in self.minimum_price_dict.keys():
+            price = self.minimum_price_dict[prefix]["price"]
+            operator = self.minimum_price_dict[prefix]["operator"]
 
-        if len(prefix) == 1:
-            current_node.cheapest_operator = operator
-            current_node.cheapest_price = price
+            assert prefix and price and operator
+            current_node = self.first_digit[int(prefix[0])]
 
-        for i in range(1, len(prefix)):
-            digit = int(prefix[i])
-            if digit in current_node.next_digit:
-                current_node = current_node.next_digit[digit]
-            else:
-                current_node.next_digit[digit] = DigitNode(digit)
-                current_node = current_node.next_digit[digit]
-            if i == len(prefix)-1:
+            if len(prefix) == 1:
                 current_node.cheapest_operator = operator
                 current_node.cheapest_price = price
 
-    print(query(first_digit, "144234123423"))
+            for i in range(1, len(prefix)):
+                digit = int(prefix[i])
+                if digit in current_node.next_digit:
+                    current_node = current_node.next_digit[digit]
+                else:
+                    current_node.next_digit[digit] = DigitNode(digit)
+                    current_node = current_node.next_digit[digit]
+                if i == len(prefix)-1:
+                    current_node.cheapest_operator = operator
+                    current_node.cheapest_price = price
+
+    def query(self, phone_number) -> tuple[OperatorPrice, int]:
+        assert phone_number
+        node = self.first_digit[int(phone_number[0])]
+        for digit in phone_number[1:]:
+            digit = int(digit)
+            if digit in node.next_digit:
+                node = node.next_digit[digit]
+            else:
+                return {
+                    "operator": node.cheapest_operator,
+                    "price": node.cheapest_price
+                }, node.digit
+
+
+if __name__ == "__main__":
+    a = CheapestOpFinder(CSV_FOLDER)
+    logger.info(a.query("144234123423"))
